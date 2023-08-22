ea347446dd09679634685f527a14c8152edb6f01
MattBuchs <mattbuchs25@gmail.com>
2023-08-22 19:04:58 +0200

    console.log removal in middleware

--- app/validation/authToken.middleware.js
+++ app/validation/authToken.middleware.js
@@ -3,16 +3,11 @@ import jwt from 'jsonwebtoken';
 // this middleware is used to verify the token
 export default function authenticateToken(req, res, next) {
     const token = req.headers.authorization;
-    console.log(token);
-    console.log(process.env.SECRET_KEY);
 
     if (token === undefined) return res.status(401).json({ error: 'Token undefined' });
 
     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
-        if (err) {
-            console.error('Token verification error:', err);
-            return res.status(403).json({ error: 'Token verification failed' });
-        }
+        if (err) return res.status(403).json({ error: 'Token verification failed' });
 
         req.user = user;
         next();
