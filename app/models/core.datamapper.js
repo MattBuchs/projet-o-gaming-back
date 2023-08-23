c711ea14359abb5e3e0bf6ae6a354886bd43f1f1
MattBuchs <mattbuchs25@gmail.com>
2023-08-23 12:57:21 +0200

    fix findByPk function

--- app/models/core.datamapper.js
+++ app/models/core.datamapper.js
@@ -13,7 +13,7 @@ export default class CoreDatamapper {
     }
 
     async findByPk(id) {
-        const result = await this.client.query(`SELECT * FROM "${this.tableName}" WHERE id = ${id}`);
+        const result = await this.client.query(`SELECT * FROM "${this.tableName}" WHERE id = $1`, [id]);
         return result.rows[0];
     }
 
