4d7b0224c65758f56b7a452b6cf87e6ba0869092
Zachary <zacharyblundell@live.com>
2023-08-24 17:30:40 +0200

    switched to use shorthand in the state object
    changed
    games:  to just games

--- app/controllers/games.controller.js
+++ app/controllers/games.controller.js
@@ -6,14 +6,15 @@ export default {
     async getGames(req, res) {
         try {
             const games = await datamappers.gameDatamapper.findAll();
-            return res.json({ games: `${games}` });
+            return res.json({ games });
         } catch (err) {
             // code 23505 = unique_violation
             if (err.code === '23505') {
                 throw new UserInputError(err);
             } else {
-                return res.status(500).json({ error: `Internal Server Error: ${DatabaseError(err)}` });
                 throw new DatabaseError(err);
+                // return res.status(500)
+                // .json({ error: `Internal Server Error: ${DatabaseError(err)}`});
             }
         }
     },
