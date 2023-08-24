a041904392b36c744e6205f2da5f0a7979f3cd16
Zachary <zacharyblundell@live.com>
2023-08-24 16:11:48 +0200

    added pg connect status console log

--- app/helpers/db.js
+++ app/helpers/db.js
@@ -3,7 +3,13 @@ import pg from 'pg';
 const { Client } = pg;
 
 const client = new Client();
-
-client.connect();
+// await client.connect();
+client.connect((error) => {
+    if (error) {
+        console.log('Error connecting to database', error);
+    } else {
+        console.log('pg: successfully connected to the database');
+    }
+});
 
 export default client;
