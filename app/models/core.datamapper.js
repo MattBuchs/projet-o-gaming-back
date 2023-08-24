33a934136128c83b1667f3b829b901c836476abb
Zachary <zacharyblundell@live.com>
2023-08-24 17:28:11 +0200

    removed console log statments

--- app/models/core.datamapper.js
+++ app/models/core.datamapper.js
@@ -21,9 +21,7 @@ export default class CoreDatamapper {
         const fields = Object.keys(inputData);
         const values = Object.values(inputData);
 
-        console.log('values: ', values);
         const placeholders = values.map((_, index) => `$${index + 1}`);
-        console.log('placeholders: ', placeholders);
 
         const result = await this.client.query(
             `
