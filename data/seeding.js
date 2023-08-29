5b5de292c6de988ed1b5ef6ac58aa2678a922d81
Zachary <zacharyblundell@live.com>
2023-08-29 14:08:09 +0200

    added more realist categories and cleaned up

--- data/seeding.js
+++ data/seeding.js
@@ -7,7 +7,7 @@ const client = new Client({
     host: 'localhost',
     database: 'o-gaming',
 });
-// await client.connect();
+
 client.connect((error) => {
     if (error) {
         console.log('Error in seeding with the database connection', error);
@@ -16,6 +16,14 @@ client.connect((error) => {
     }
 });
 
+async function accessDatabase(query, values) {
+    try {
+        await client.query(query, values);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
 const NumberOfFakeEntries = 10;
 
 // TAG seeding --------------------------------
@@ -23,14 +31,9 @@ for (let i = 0; i < NumberOfFakeEntries; i += 1) {
     // faker data
     const randomName = faker.word.noun();
     // insert data into database
-    try {
-        await client.query(
-            'INSERT INTO tag(title) VALUES($1);',
-            [randomName],
-        );
-    } catch (err) {
-        console.error(err);
-    }
+    const query = 'INSERT INTO tag(title) VALUES($1);';
+    const values = [randomName];
+    await accessDatabase(query, values);
 }
 
 // PLATFORM seeding --------------------------------
@@ -52,16 +55,11 @@ const consoles = [
     'Atari 2600',
 ];
 
-for (let i = 0; i < NumberOfFakeEntries; i += 1) {
+for (let i = 0; i < consoles.length; i += 1) {
     // insert data into database
-    try {
-        await client.query(
-            'INSERT INTO platform(name) VALUES($1);',
-            [consoles[i]],
-        );
-    } catch (err) {
-        console.error(err);
-    }
+    const query = 'INSERT INTO platform(name) VALUES($1);';
+    const values = [consoles[i]];
+    await accessDatabase(query, values);
 }
 
 // ROLE seeding --------------------------------
@@ -76,23 +74,28 @@ try {
 } catch (err) {
     console.error(err);
 }
-// mot de passe pour le wifi 64PTKTKE 
 
 // CATEGORY seeding --------------------------------
-const categories = ['FPS', 'Survial', 'RPG', 'Turn-based']
-for (let i = 0; i < NumberOfFakeEntries; i += 1) {
+const categories = [
+    'Action',
+    'Adventure',
+    'Casual',
+    'Racing',
+    'Independent',
+    'Massive Multiplayer',
+    'RPG',
+    'Simulation',
+    'Sport',
+    'Strategy',
+];
+
+for (let i = 0; i < categories.length; i += 1) {
     // faker data
-    const randomName = faker.word.noun();
     const randomColor = faker.color.rgb();
     // insert data into database
-    try {
-        await client.query(
-            'INSERT INTO "category" (name, color) VALUES($1, $2);',
-            [randomName, randomColor],
-        );
-    } catch (err) {
-        console.error(err);
-    }
+    const query = 'INSERT INTO "category" (name, color) VALUES($1, $2);';
+    const values = [categories[i], randomColor];
+    await accessDatabase(query, values);
 }
 
 // USER seeding --------------------------------
@@ -103,27 +106,20 @@ for (let i = 0; i < NumberOfFakeEntries; i += 1) {
     const randomPassword = faker.internet.password();
     const randomAvatar = faker.internet.url();
     let roleID;
-    // fist three users are devs
-    if (i < 3) {
+    if (i < 3) { // fist three users are devs
         roleID = 1;
     } else roleID = 2; // else they are players
     // insert data into database
-    try {
-        await client.query(
-            'INSERT INTO "user" (username, email, password, avatar, role_id) VALUES($1, $2, $3, $4, $5);',
-            [randomName, randomEmail, randomPassword, randomAvatar, roleID],
-        );
-    } catch (err) {
-        console.error(err);
-    }
+    const query = 'INSERT INTO "user" (username, email, password, avatar, role_id) VALUES($1, $2, $3, $4, $5);';
+    const values = [randomName, randomEmail, randomPassword, randomAvatar, roleID];
+    await accessDatabase(query, values);
 }
 
 // GAME seeding --------------------------------
 for (let i = 0; i < NumberOfFakeEntries; i += 1) {
     // faker data
     let randomName = faker.internet.domainWord();
-    // capitlize the first letter
-    randomName = randomName[0].toUpperCase() + randomName.substring(1);
+    randomName = randomName[0].toUpperCase() + randomName.substring(1); // capitlize first letter
     const randomDescripion = faker.lorem.paragraph({ min: 3, max: 6 });
     const randomPictureUrl = faker.internet.url();
     const randomExternalLink = faker.internet.url();
@@ -131,20 +127,18 @@ for (let i = 0; i < NumberOfFakeEntries; i += 1) {
     let devID;
     if (i <= 3) devID = 1; // first 5 devID will be 1
     else devID = 2; // last 5 devID will be 2
-
     // insert data into database
-    try {
-        await client.query(
-            'INSERT INTO game(name, description, picture, external_link, release_date, user_id) VALUES($1, $2, $3, $4, $5, $6);',
-            [randomName, randomDescripion, randomPictureUrl, randomExternalLink, randomReleaseDate, devID],
-        );
-    } catch (err) {
-        console.error(err);
-    }
+    const query = 'INSERT INTO game(name, description, picture, external_link, release_date, user_id) VALUES($1, $2, $3, $4, $5, $6);';
+    const values = [
+        randomName,
+        randomDescripion,
+        randomPictureUrl,
+        randomExternalLink,
+        randomReleaseDate,
+        devID,
+    ];
+    await accessDatabase(query, values);
 }
-const statuses = [
-    'read', 'accepted', 'duplicate', 'working on it', 'refused', 'fixed',
-];
 
 // SUGGESTION seeding --------------------------------
 for (let i = 0; i < NumberOfFakeEntries; i += 1) {
@@ -162,14 +156,12 @@ for (let i = 0; i < NumberOfFakeEntries; i += 1) {
     // insert data into database
     const query = 'INSERT INTO "suggestion" (title, description, published_at, user_id, game_id) VALUES($1, $2, $3, $4, $5);';
     const values = [randomTitle, randomDescription, randomPublishDate, randomUserID, randomGameID];
-    try {
-        await client.query(query, values);
-    } catch (err) {
-        console.error(err);
-    }
+    await accessDatabase(query, values);
 }
 
 // ISSUE seeding --------------------------------
+const statuses = ['read', 'accepted', 'duplicate', 'working on it', 'refused', 'fixed'];
+
 for (let i = 0; i < NumberOfFakeEntries; i += 1) {
     // faker data
     const randomTitle = faker.lorem.lines(1);
@@ -177,9 +169,8 @@ for (let i = 0; i < NumberOfFakeEntries; i += 1) {
     // this will prevent going out of bounds of the status array
     const statusNumber = i % statuses.length;
     const randomStatus = statuses[statusNumber];
-
-    let isMinorImportanceBool;
     const assignedToEmail = faker.internet.email();
+    let isMinorImportanceBool;
     let randomOnlineStat;
     let randomFrequency;
     let isPublic;
@@ -194,10 +185,8 @@ for (let i = 0; i < NumberOfFakeEntries; i += 1) {
         isPublic = false;
         randomFrequency = 'Always';
     }
-
     const randomReplication = faker.lorem.paragraph({ min: 1, max: 2 });
     const randomPublishDate = faker.date.past({ years: 10 }); // date within the last ten years
-
     let randomUserID;
     if (i <= 3) { // a number between 4-10 these corruspond to players
         randomUserID = i + 3;
@@ -208,12 +197,22 @@ for (let i = 0; i < NumberOfFakeEntries; i += 1) {
     const randomPlatformID = i + 1;
     // insert data into database
     const query = 'INSERT INTO "issue" (title, description, status, is_minor, assign_to, is_public, is_online, frequency, replication, published_at, user_id, game_id, platform_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13);';
-    const values = [randomTitle, randomDescription, randomStatus, isMinorImportanceBool, assignedToEmail, isPublic, randomOnlineStat, randomFrequency, randomReplication, randomPublishDate, randomUserID, randomGameID, randomPlatformID];
-    try {
-        await client.query(query, values);
-    } catch (err) {
-        console.error(err);
-    }
+    const values = [
+        randomTitle,
+        randomDescription,
+        randomStatus,
+        isMinorImportanceBool,
+        assignedToEmail,
+        isPublic,
+        randomOnlineStat,
+        randomFrequency,
+        randomReplication,
+        randomPublishDate,
+        randomUserID,
+        randomGameID,
+        randomPlatformID,
+    ];
+    await accessDatabase(query, values);
 }
 
 // COMMENT seeding
@@ -223,14 +222,9 @@ for (let i = 0; i < NumberOfFakeEntries; i += 1) {
     const devID = (i % 3) + 1; // this will only use the first three users who are devs
     const randomIssueID = i + 1;
     // insert data into database
-    try {
-        await client.query(
-            'INSERT INTO "comment" (description, user_id, issue_id) VALUES($1, $2, $3);',
-            [randomDescription, devID, randomIssueID],
-        );
-    } catch (err) {
-        console.error(err);
-    }
+    const query = 'INSERT INTO "comment" (description, user_id, issue_id) VALUES($1, $2, $3);';
+    const values = [randomDescription, devID, randomIssueID];
+    await accessDatabase(query, values);
 }
 
 await client.end();
