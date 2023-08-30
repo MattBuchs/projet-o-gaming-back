2111f55d4503604b0844ac39b8bb2692e4c80dcb
MattBuchs <mattbuchs25@gmail.com>
2023-08-30 11:13:15 +0200

    updated authToken.middleware

--- app/validation/authToken.middleware.js
+++ app/validation/authToken.middleware.js
@@ -2,9 +2,10 @@ import jwt from 'jsonwebtoken';
 
 // this middleware is used to verify the token
 export default function authenticateToken(req, res, next) {
-    const token = req.headers.authorization;
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(' ')[1];
 
-    if (token === undefined) return res.status(401).json({ error: 'Token undefined' });
+    if (!token) return res.status(401).json({ error: 'Token undefined' });
 
     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
         if (err) return res.status(403).json({ error: 'Token verification failed' });
