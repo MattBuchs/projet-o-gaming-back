6598fcabe724b923711c1994714a0c4f7f4ca5bb
MattBuchs <mattbuchs25@gmail.com>
2023-09-05 15:45:48 +0200

    doc: All get

--- app/routers/index.router.js
+++ app/routers/index.router.js
@@ -47,13 +47,14 @@ router.route('/games')
      * GET /games
      * @summary Get all games
      * @return {Array.<Game>} 200 - Array of games
+     * @return {Error}  404 - Games not found
      * @return {Error}  500 - Internal server error
      */
     .get(controllerGame.getAllGames);
 
 router.route('/games/game/:id_game')
     /**
-     * GET /games/game/:id_game
+     * GET /games/game/{id_game}
      * @summary Get a game
      * @param {integer} id_game.path.required - Game id
      * @return {Game} 200 - Game
@@ -66,48 +67,132 @@ router.route('/games/game/:id_game')
 
 /* Issues */
 router.route('/games/game/:id_game/issues')
+    /**
+     * GET /games/game/{id_game}/issues
+     * @summary Get all issues from a game
+     * @param {integer} id_game.path.required - Game id
+     * @return {Array.<Issue>} 200 - Array of issues
+     * @return {Error}  404 - Game not found
+     * @return {Error}  500 - Internal server error
+     */
     .get(controllerIssue.getAllIssues)
     .post(authenticateToken, controllerIssue.createIssue);
 
-router.route('/games/game/:id_game/issue/:id_issue')
+router.route('/issue/:id_issue')
+    /**
+     * GET /issue/{id_issue}
+     * @summary Get an issue
+     * @param {integer} id_issue.path.required - Issue id
+     * @return {Issue} 200 - Issue
+     * @return {Error}  404 - Issue not found
+     * @return {Error}  500 - Internal server error
+     */
     .get(controllerIssue.getOneIssue)
     .patch(authenticateToken, controllerIssue.updateIssue)
     .delete(authenticateToken, controllerIssue.deleteIssue);
 
 /* Suggestions */
 router.route('/games/game/:id_game/suggestions')
+    /**
+     * GET /games/game/{id_game}/suggestions
+     * @summary Get all suggestions from a game
+     * @param {integer} id_game.path.required - Game id
+     * @return {Array.<Suggestion>} 200 - Array of suggestions
+     * @return {Error}  404 - Game not found
+     * @return {Error}  500 - Internal server error
+     */
     .get(controllerSuggestion.getAllSuggestions)
     .post(authenticateToken, controllerSuggestion.createSuggestion);
 
-router.route('/games/game/:id_game/suggestion/:id_suggestion')
+router.route('/suggestion/:id_suggestion')
+    /**
+     * GET /suggestion/{id_suggestion}
+     * @summary Get a suggestion
+     * @param {integer} id_suggestion.path.required - Suggestion id
+     * @return {Suggestion} 200 - Suggestion
+     * @return {Error}  404 - Suggestion not found
+     * @return {Error}  500 - Internal server error
+     */
     .get(controllerSuggestion.getOneSuggestion)
     .patch(authenticateToken, controllerSuggestion.updateSuggestion)
     .delete(authenticateToken, controllerSuggestion.deleteSuggestion);
 
 /* Categories */
 router.route('/categories')
+    /**
+     * GET /categories
+     * @summary Get all categories
+     * @return {Array.<Category>} 200 - Array of categories
+     * @return {Error}  404 - Categories not found
+     * @return {Error}  500 - Internal server error
+     */
     .get(controllerUtils.getAllCategories);
 
 /* Users */
 router.route('/users')
+    /**
+     * GET /users
+     * @summary Get all users
+     * @return {Array.<Users>} 200 - Array of users
+     * @return {Error}  404 - Users not found
+     * @return {Error}  500 - Internal server error
+     */
     .get(controllerUser.getAllUsers);
 
 router.route('/user/:id_user')
+    /**
+     * GET /user/{id_user}
+     * @summary Get a user
+     * @param {integer} id_user.path.required - User id
+     * @return {User} 200 - User
+     * @return {Error}  404 - User not found
+     * @return {Error}  500 - Internal server error
+     */
     .get(controllerUser.getOneUser)
     .patch(authenticateToken, controllerUser.updateUser);
 
 /* Search */
 router.route('/search')
+    /**
+     * GET /search
+     * @summary Search
+     * @param {string} search.query.required - Search
+     * @return {Array.<Search>} 200 - Array of search
+     * @return {Error}  404 - Search not found
+     * @return {Error}  500 - Internal server error
+     */
     .get(controllerSearch.getSearch);
 
 /* Utils */
 router.route('/platforms')
+    /**
+     * GET /platforms
+     * @summary Get all platforms
+     * @return {Array.<Platforms>} 200 - Array of platforms
+     * @return {Error}  404 - Platforms not found
+     * @return {Error}  500 - Internal server error
+     */
     .get(controllerUtils.getAllPlatforms);
 
 router.route('/games/game/:id_game/tags')
+    /**
+     * GET /games/game/{id_game}/tags
+     * @summary Get all tags from a game
+     * @param {integer} id_game.path.required - Game id
+     * @return {Array.<Tags>} 200 - Array of tags of a game
+     * @return {Error}  404 - Game not found
+     * @return {Error}  500 - Internal server error
+     */
     .get(controllerUtils.getTagsFromGame);
 
 router.route('/tags')
+    /**
+     * GET /tags
+     * @summary Get all tags
+     * @return {Array.<Tags>} 200 - Array of tags
+     * @return {Error}  404 - Tags not found
+     * @return {Error}  500 - Internal server error
+     */
     .get(controllerUtils.getAllTags);
 
 export default router;
--- app/services/auth.service.js
+++ app/services/auth.service.js
@@ -33,8 +33,85 @@ export default {
      * @property {string} picture - The picture of the game
      * @property {string} external_link - The external link of the game
      * @property {string} release_date - The release date of the game
+     * @format date
      * @property {integer} user_id - The user id of the game
      * @property {string} author - The author of the game
      * @property {Array<string>} categories - The categories of the game
+     * @property {string} release_date.format - The release_date format is Timestamptz
+     */
+
+    /**
+     * @typedef {object} Issue
+     * @property {integer} id - The id of the issue
+     * @property {string} title - The title of the issue
+     * @property {string} description - The description of the issue
+     * @property {string} status - The status of the issue
+     * @property {boolean} is_minor - The is_minor of the issue
+     * @property {string} assign_to - The assign_to of the issue
+     * @property {boolean} is_public - The is_public of the issue
+     * @property {boolean} is_online - The is_online of the issue
+     * @property {string} frequency - The frequency of the issue
+     * @property {string} replication - The replication of the issue
+     * @property {string} published_at - The published_at of the issue
+     * @property {integer} user_id - The user_id of the issue
+     * @property {string} author - The author of the issue
+     * @property {integer} platform_id - The platform_id of the issue
+     * @property {string} platform - The platform of the issue
+     * @property {Array<string>} tags - The tags of the issue
+     */
+
+    /**
+     * @typedef {object} Suggestion
+     * @property {integer} id - The id of the suggestion
+     * @property {string} title - The title of the suggestion
+     * @property {string} description - The description of the suggestion
+     * @property {string} published_at - The published_at of the suggestion
+     * @property {integer} user_id - The user_id of the suggestion
+     * @property {string} author - The author of the suggestion
+     * @property {integer} platform_id - The platform_id of the suggestion
+     * @property {string} platform - The platform of the suggestion
+     */
+
+    /**
+     * @typedef {object} Users
+     * @property {integer} id - The id of the user
+     * @property {string} username - The username of the user
+     * @property {string} email - The email of the user
+     * @property {string} avatar - The avatar of the user
+     */
+
+    /**
+     * @typedef {object} User
+     * @property {integer} id - The id of the user
+     * @property {string} username - The username of the user
+     * @property {string} email - The email of the user
+     * @property {string} avatar - The avatar of the user
+     * @property {integer} role - The role of the user
+     * @property {Array<Game>} games - The games of the user
+     */
+
+    /**
+     * @typedef {object} Category
+     * @property {integer} id - The id of the category
+     * @property {string} name - The name of the category
+     * @property {string} color - The color of the category
+     */
+
+    /**
+     * @typedef {object} Search
+     * @property {Array<Game>} games - The games of the search
+     * @property {Array<Users>} users - The users of the search
+     */
+
+    /**
+     * @typedef {object} Platforms
+     * @property {integer} id - The id of the platform
+     * @property {string} name - The name of the platform
+     */
+
+    /**
+     * @typedef {object} Tags
+     * @property {integer} id - The id of the tag
+     * @property {string} title - The title of the tag
      */
 };
