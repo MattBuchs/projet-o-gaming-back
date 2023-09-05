9839f0f17da2972dadc571ab85f9c7bfd24bc942
MattBuchs <mattbuchs25@gmail.com>
2023-09-05 10:35:53 +0200

    added is_online and user_id to findIssuesWithGame

--- app/models/issue.datamapper.js
+++ app/models/issue.datamapper.js
@@ -22,10 +22,10 @@ export default class IssueDatamapper extends CoreDatamapper {
             "user"."username" AS author,
             "game"."name" AS game,
             "platform"."name" AS platform, 
-        CASE
+            CASE
                 WHEN COUNT("tag"."id") > 0 THEN JSON_AGG("tag"."title")
                 ELSE '[]'::json
-        END AS tags
+            END AS tags
         FROM "issue"
         JOIN "user" ON "user"."id" = "issue"."user_id"
         JOIN "platform" ON "platform"."id" = "issue"."platform_id"
@@ -47,24 +47,27 @@ export default class IssueDatamapper extends CoreDatamapper {
 
     async findIssuesWithGame(gameId) {
         const result = await this.client.query(`
-            SELECT
-                "issue"."id",
-                "issue"."title",
-                "issue"."status",
-                "user"."username" AS "author",
+        SELECT
+            "issue"."id",
+            "issue"."title",
+            "issue"."status",
+            "issue"."is_online",
+            "user"."id" AS user_id,
+            "user"."username" AS "author",
             CASE
                 WHEN COUNT("tag"."id") > 0 THEN JSON_AGG("tag"."title")
                 ELSE '[]'::json
             END AS tags
-            FROM "issue"
-            JOIN "user" ON "user"."id" = "issue"."user_id"
-            LEFT JOIN "issue_has_tag" ON "issue_has_tag"."issue_id" = "issue"."id"
-            LEFT JOIN "tag" ON "tag"."id" = "issue_has_tag"."tag_id"
-            WHERE "issue"."game_id" = $1
-            GROUP BY
-                "issue"."id",
-                "issue"."status",
-                "user"."username";
+        FROM "issue"
+        JOIN "user" ON "user"."id" = "issue"."user_id"
+        LEFT JOIN "issue_has_tag" ON "issue_has_tag"."issue_id" = "issue"."id"
+        LEFT JOIN "tag" ON "tag"."id" = "issue_has_tag"."tag_id"
+        WHERE "issue"."game_id" = $1
+        GROUP BY
+            "issue"."id",
+            "issue"."status",
+            "user"."id",
+            "user"."username"
         `, [gameId]);
 
         return result.rows;
