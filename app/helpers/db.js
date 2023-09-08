3b11c0d2f5b634337c4ad61b2ade6a31c2bb07ff
MattBuchs <mattbuchs25@gmail.com>
2023-09-08 14:12:45 +0200

    added ssl

--- app/helpers/db.js
+++ app/helpers/db.js
@@ -5,6 +5,9 @@ const { Client } = pg;
 
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 // await client.connect();
